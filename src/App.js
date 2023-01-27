1674af76b9d66d193b6dd047fee2347add201cf9
MilanTibadiya <tibadiyamilan@gmail.com>
2023-01-27 11:25:33 +0530

    01-Building the header of restaurant app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
