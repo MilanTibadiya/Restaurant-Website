e055f578d88b087cebba3771b3c5c9231473ef8f
MilanTibadiya <tibadiyamilan@gmail.com>
2023-01-30 17:09:18 +0530

    08-(Showing and Enabling the delete of Cart Items)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,56 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
+import { useContext } from "react";
+
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
-    const cartItem = <ul className={classes['cart-items']}>
-        {[{ id: 'c1', name: 'Sushi', amount: 2, price:12.99},
-    ].map((item) => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
 
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItem}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>35.65</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
-            </div>
-        </Modal>
-    );
+    // console.log(cartCtx.totalAmount);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+      cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem(item);
+    };
+
+  const cartItem = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem 
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}  
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItem}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button-alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
